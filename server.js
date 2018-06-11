2351328ec5672038b9b95432b78bdcb4abbd6030
alexreu <alexxxx.974@gmail.com>
2018-06-11 11:31:59 +0400

    mise a jour des fichiers json

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
  * Initialisation du serveur + ajout d'express
  */
 const express = require('express');
+var fs = require('fs');
 const app = express();
 const port = 3012;
+
+var parser = require('json-parser');
 
 app.use(express.static('static'));
 /**
@@ -17,7 +20,7 @@ app.get('/', function (req, res) {
  * Route vers le fichier groups.json
  */
 app.get('/groups', function(req, res){
-    res.sendFile(__dirname + ('/groups.json'))
+    res.sendFile(__dirname + '/groups.json')
 });
 /**
  * Route vers date du match
@@ -29,5 +32,16 @@ app.get('/groups', function(req, res){
 app.listen(port, function(){
     console.log('server ON');
 })
+
+
+
+fs.readFile('groups.json', 'utf8', function(error, data) {
+    // console.log(data);
+    var obj = JSON.parse(data);
+    console.log(obj.groups.a);
+    for(var i =0; i<obj.length; i++){
+        
+    }
+});
 
 
