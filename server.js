3dc73b7562d07aaec2af3c5c165943066909f0af
Adama Diallo <adamadial58@me.com>
2018-06-12 11:00:05 +0400

    suppression du dossier node_modules

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 var parser = require('json-parser');
 var fs = require('fs');
 const app = express();
-const port = 3015;
+const port = 3012;
 
 app.use(express.static('static'));
 
