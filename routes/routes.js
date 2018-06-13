cc413e06c49c161c30826984855ad20d1c753895
alexreu <alexxxx.974@gmail.com>
2018-06-13 13:59:53 +0400

    creation du module score.js et score.json

--- routes/routes.js
+++ routes/routes.js
@@ -23,5 +23,10 @@ router.use('/get_groups', groups);
  */
 var stadium = require('../stadiums.js');
 router.use('/get_stadiums', stadium);
+/**
+ * affiches les scores des pays de la worldcup
+ */
+var score = require('../score.js');
+router.use('/get_scores', score);
 
 module.exports = router;
