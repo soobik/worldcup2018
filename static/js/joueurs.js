8fb86076bd69b50ed32bff3247cb6ffd505964b1
nturpin <nova.t974@gmail.com>
2018-06-14 09:49:17 +0400

    affichage joueurs avec équipe

--- static/js/joueurs.js
+++ static/js/joueurs.js
@@ -1,19 +1,28 @@
 
 
     $(document).ready(function() {
+        var equipes = [];
+
+        //recupere les equipes
+        services("equipes", function (data) {
+            equipes = data;
+            //console.log(equipes[5].flag);
+        })
+
+
         $.ajax({
             type: 'GET',
             url: 'http://localhost:4002/get_joueurs',
             success: function (listeJoueurs) {
                 for (var i = 0; i < listeJoueurs.joueurs.length; i++) {
-                    
+                    //recupere les joueurs
                     var player = "";
                    
                      if(listeJoueurs.joueurs[i] === 1){
-                        player = joueurs.nomJoueur + joueurs.pays;
+                        player = joueurs.nomJoueur + joueurs.id_equipe;
                      }
 
-                    $('#liste_joueurs').append('<li class="list-group-item"><h4>' + listeJoueurs.joueurs[i].nomJoueur + '</h4> ' + listeJoueurs.joueurs[i].pays+ '</li>');
+                    $('#liste_joueurs').append('<li class="list-group-item"><h4>' + listeJoueurs.joueurs[i].nomJoueur + '</h4> ' +  '<img class="player_flag" src="'+equipes[listeJoueurs.joueurs[i].id_equipe].flag+'" /></li>');
                 };
                 
             },
